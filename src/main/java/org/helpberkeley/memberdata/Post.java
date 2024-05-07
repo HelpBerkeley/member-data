071eb4ff252c36da9085624aa50690f5318323a7
Amosnomor <ds.helpberkeley@asif.com>
2024-05-07 10:57:29 -0700

    Maven dependency updates (#6)
    * Added/used maven versions build plugin to update
    to latest maven dependency versions.
    
    Updates for API changes in json-io.
    
    * Updated json-io usage after clarifying proper usage:
    https://github.com/jdereg/json-io/issues/282
    
    * Fixed typos.

--- src/main/java/org/helpberkeley/memberdata/Post.java
+++ src/main/java/org/helpberkeley/memberdata/Post.java
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2020 helpberkeley.org
+// Copyright (c) 2020-2024 helpberkeley.org
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -21,10 +21,8 @@
 //
 package org.helpberkeley.memberdata;
 
-import com.cedarsoftware.util.io.JsonWriter;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.cedarsoftware.io.JsonIo;
+import com.cedarsoftware.io.WriteOptionsBuilder;
 
 public class Post {
 
@@ -35,8 +33,7 @@ public class Post {
 
     String toJson() {
 
-        Map<String, Object> options = new HashMap<>();
-        options.put(JsonWriter.TYPE, Boolean.FALSE);
-        return JsonWriter.objectToJson(this, options);
+        return JsonIo.toJson(this,
+                new WriteOptionsBuilder().showTypeInfoNever().build());
     }
 }
